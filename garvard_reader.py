8be690342f6f8b0a04c5ad7cf8778667e4a12297
gamblesser <danyagrishin474@gmail.com>
2022-11-28 23:32:44 +0300

    v 1

---
+++ garvard_reader.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from requests_html import HTMLSession
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[2].id)
+engine.setProperty('rate', 90)
+engine.runAndWait()
+
+def speak(str):
+    engine.say(str)
+    engine.runAndWait()
+
+def takeSomelements(list_,count):
+    while True:
+        try:    
+            return list_[:count]
+        except:
+            count-=1
+            continue
+
+words = pd.read_excel('1000 most used english words.xlsx')['WORDS']
+start = int(input('Start point\n'))
+for k,wordForSearch in enumerate(words[start-2:]):
+    print('-'*10)
+    print(wordForSearch,'   ',start+k,end='\n\n')
+    count = 0
+    speak(f'Word . {wordForSearch} . in number {start+k}')
+    url = f'https://dictionary.cambridge.org/ru/словарь/англо-русский/{wordForSearch}'
+    session = HTMLSession()
+    r = session.get(url)
+    for meaning,examples in zip(r.html.xpath('//div[@class="def ddef_d db"]'),r.html.xpath('//div[@class="def-body ddef_b"]')):
+        count+=1
+        speak(f'{count} meaning')
+        
+        
+        rusMe= examples.xpath('//span')[0].text
+        
+        example= examples.xpath('//div')[1:]
+        example= '.\n'.join([f"· {example_.text.strip('.')}" for example_ in takeSomelements(example,10)]).replace('/',' ').replace('\\',' ')+'.'
+        print(meaning.text)
+        print(rusMe)
+        print(f'{example}',end='\n\n')
+        
+        
+        speak(meaning.text)
+        engine.setProperty('voice', voices[0].id)
+        engine.setProperty('rate', 130)
+        tts = speak(rusMe)
+        engine.setProperty('voice', voices[2].id)
+        speak('EXAMPLES')
+        engine.setProperty('rate', 90)
+        tts = speak(example)
---
+++ ss.bat
@@ -0,0 +1,2 @@
+echo C:\Users\danya\Documents\GitHub\garvard_reader
+start py C:\Users\danya\Documents\GitHub\garvard_reader\garvard_reader.py
